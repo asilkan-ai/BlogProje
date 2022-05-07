ec53800761427a9af3a0dd04b0aae181ed2a3c2f
asilkan <asilkan.klc@gmail.com>
2022-05-07 17:32:29 +0300

    blog degistirildi

---
+++ MvcProje/Controllers/BlogController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult BlogList()
+        {
+            return PartialView();
+        }
+        public PartialViewResult FeaturedPosts()
+        {
+            return PartialView();
+        }
+        public PartialViewResult OtherFeaturedPosts()
+        {
+            return PartialView();
+        }
+        public PartialViewResult MailSubscribe()
+        {
+            return PartialView();
+        }
+
+
+
+    }
+}
